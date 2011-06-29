41de3cc61ae893ba02517bd0c0e1db9a36dba96a
unknown <Luke@.(none)>
2011-06-29 10:00:32 +0800

    Added 404.html and 500.html

--- settings.py
+++ settings.py
@@ -4,10 +4,11 @@ import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+DIRNAME =os.path.dirname(__file__)
+
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
     ('Luke Chen', 'nuemail@gmail.com')
-
 )
 
 MANAGERS = ADMINS
@@ -55,20 +56,20 @@ USE_L10N = True
 if DEBUG:
     # Absolute filesystem path to the directory that will hold user-uploaded files.
     # Example: "/home/media/media.lawrence.com/media/"
-    MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'uploaded_files')
-
-    # URL that handles the media served from MEDIA_ROOT. Make sure to use a
-    # trailing slash.
-    # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-    MEDIA_URL = '/uploaded_files/'
+    MEDIA_ROOT = os.path.join(DIRNAME, 'uploaded_files')
 else:
-    MEDIA_ROOT = '/var/www/wufulab.com'
+    MEDIA_ROOT = '/var/www/wufulab.com/uploaded_files'
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/uploaded_files/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/var/www/wufulab.com/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -84,7 +85,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__), 'static'),
+    os.path.join(DIRNAME, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -130,7 +131,7 @@ ROOT_URLCONF = 'Kavalan_Management_System.urls'
 
 TEMPLATE_DIRS = (
 
-        (os.path.join(os.path.dirname(__file__), 'templates')),
+        (os.path.join(DIRNAME, 'templates')),
 
 )
 
