8c401f79e1faa44cdf6017ac4b46da646da7731a
Luke Chen <nuemail@gmail.com>
2011-07-05 23:55:46 +0800

    Branch fix

---
+++ api/handlers.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponseRedirect
+from django.conf import settings
+
+class DefaultAuthenticationHandler(object):
+    def is_authenticated(self, request):
+        return request.user.is_authenticated()
+
+    def challenge(self):
+        return HttpResponseRedirect(settings.LOGIN_URL)
+        
+  
