47e31d6d5ea88ecc2d005625a3c128869e85edc9
Luke Yu-Po Chen <nuemail@gmail.com>
2012-01-09 20:22:21 +0800

    Remove unused files. Remove migration files for the app 'accounts' to avoid errors with syncdb command.

--- kavalan/kavalan_tags/templatetags/kavalan_tags.py
+++ kavalan/kavalan_tags/templatetags/kavalan_tags.py
@@ -1,9 +1,5 @@
-#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-
 from django import template
-from django.utils.translation import ugettext
-
 register = template.Library()
 
 @register.inclusion_tag('kavalan_tags/bootstrap_fields.html')
