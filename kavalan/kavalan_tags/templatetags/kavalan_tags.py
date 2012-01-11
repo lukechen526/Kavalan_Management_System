200e16037ccd5d6c9958a2e203d6f0aee698b1ba
Luke Yu-Po Chen <nuemail@gmail.com>
2012-01-11 13:31:58 +0800

    Add 'active' tag to mark the navigation links corresponding to the request path with 'active' CSS class.

--- kavalan/kavalan_tags/templatetags/kavalan_tags.py
+++ kavalan/kavalan_tags/templatetags/kavalan_tags.py
@@ -5,3 +5,13 @@ register = template.Library()
 @register.inclusion_tag('kavalan_tags/bootstrap_fields.html')
 def bootstrap_fields(form):
     return {'bootstrap_form': form}
+
+
+@register.simple_tag
+def active(request, pattern):
+    import re
+
+    if pattern == '/':
+        return 'active' if request.path == '/' else ''
+    else:
+        return 'active' if re.search(pattern, request.path) else ''
